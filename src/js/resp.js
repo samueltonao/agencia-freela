872432ca4aa5f93f6ddaef8f7ef61d5f68cfc0c6
samueltonao <samuel@tonao.com>
2024-11-01 11:26:00 -0300

    aprimoramento-code

--- src/js/resp.js
+++ src/js/resp.js
@@ -15,21 +15,3 @@ itemAtivo.forEach(function (item) {
     console.log(itemAtivoAtual);
   });
 });
-
-const itemAtivo2 = document.querySelectorAll(".pergunta-2");
-
-itemAtivo2.forEach(function (item) {
-  item.addEventListener("click", function () {
-    let itemAtivoAtual2 = document.querySelector(".resposta-2");
-
-    const desativar = itemAtivoAtual2.classList.contains("ativo");
-
-    itemAtivoAtual2.classList.add("ativo");
-
-    if (desativar) {
-      itemAtivoAtual2.classList.remove("ativo");
-    }
-
-    console.log(itemAtivoAtual2);
-  });
-});
